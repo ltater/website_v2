e8ff521f46623d20762c6c8e03850943143f6f0e
Larissa Tater <larissa.tater@gmail.com>
2015-11-28 14:12:46 -0600

    updated featured app model

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,6 +2,6 @@ class Project < ActiveRecord::Base
 
 	# Display a random selection of projects
 	def self.show_projects(number)
-		@projects = Project.order("RANDOM()").limit(number)
+		@projects = Project.limit(number).order("RANDOM()")
 	end
 end
